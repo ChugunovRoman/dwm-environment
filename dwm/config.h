5b1b139928c1fe48f34d026baa370abb2c3e4ff3
Chugunov Roman <Zebs-BMK@yandex.ru>
2019-05-17 10:33:14 +0300

    Updated DWM config

--- dwm/config.h
+++ dwm/config.h
@@ -44,43 +44,45 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 * xprop | grep -i class
 	 */
-	/* class            instance    title       tags mask     isfloating   monitor */
-	{ "Gnome-terminal", NULL,       NULL,       1,            0,           0 },
+	/* class            			instance    title       tags mask     isfloating   monitor */
+	{ "Gnome-terminal", 			NULL,       NULL,       1,            0,           0 },
 
-	{ "Code",           NULL,       NULL,       1 << 1,       0,           1 },
-	{ "Sublime_text",   NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "Atom", 		    NULL,       NULL,       1 << 1,       0,           -1 },
-	{ "QtCreator", 		NULL,       NULL,       1 << 1,       0,           0 },
+	{ "Code",           			NULL,       NULL,       1 << 1,       0,           1 },
+	{ "Sublime_text",   			NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "Atom", 		    			NULL,       NULL,       1 << 1,       0,           -1 },
+	{ "QtCreator", 					NULL,       NULL,       1 << 1,       0,           0 },
 
-	{ "Firefox",        NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "luakit",         NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Google-chrome",  NULL,       NULL,       1 << 2,       0,           0 },
-	{ "Chromium-browser",  NULL,       NULL,       1 << 2,       0,           0 },
-	{ "Yandex-browser-beta", NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Firefox",        			NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "luakit",         			NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Google-chrome",  			NULL,       NULL,       1 << 2,       0,           0 },
+	{ "Chromium-browser",  			NULL,       NULL,       1 << 2,       0,           0 },
+	{ "Yandex-browser-beta", 		NULL,       NULL,       1 << 2,       0,           -1 },
 
-	{ "pcmanfm-qt",     NULL,       NULL,       1 << 3,       0,           0 },
-	{ "Pcmanfm",        NULL,       NULL,       1 << 3,       0,           0 },
-	{ "Nautilus",       NULL,       NULL,       1 << 3,       0,           0 },
-	{ "Nemo",           NULL,       NULL,       1 << 3,       0,           0 },
+	{ "pcmanfm-qt",     			NULL,       NULL,       1 << 3,       0,           0 },
+	{ "Pcmanfm",        			NULL,       NULL,       1 << 3,       0,           0 },
+	{ "Nautilus",       			NULL,       NULL,       1 << 3,       0,           0 },
+	{ "Nemo",           			NULL,       NULL,       1 << 3,       0,           0 },
 
-	{ "Audacious",      NULL,       NULL,       1 << 4,       0,           0 },
-	// { "Audacity",       NULL,       NULL,       1 << 4,       0,           -1 },
-	{ "TelegramDesktop",NULL,       NULL,       1 << 4,       0,           0 },
+	{ "Audacious",      			NULL,       NULL,       1 << 4,       0,           0 },
+	// { "Audacity",       			NULL,       NULL,       1 << 4,       0,           -1 },
+	{ "TelegramDesktop",			NULL,       NULL,       1 << 4,       0,           0 },
 
-	{ "Gimp",           NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "Photoshop.exe",  NULL,       NULL,       1 << 5,       0,           -1 },
-	{ "Inkscape",       NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Gimp",           			NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Photoshop.exe",  			NULL,       NULL,       1 << 5,       0,           -1 },
+	{ "Inkscape",       			NULL,       NULL,       1 << 5,       0,           -1 },
 
-	{ "Mainwindow.py",  NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "Mainwindow.py",  			NULL,       NULL,       1 << 6,       0,           -1 },
 
-	{ "VirtualBox",     NULL,       NULL,       1 << 7,       0,           0 },
+	{ "VirtualBox",     			NULL,       NULL,       1 << 7,       0,           0 },
 
-	{ "qBittorrent",    NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "qBittorrent",    			NULL,       NULL,       1 << 8,       0,           -1 },
 
-	{ "bd",    			NULL,       NULL,       1 << 1,       1,           -1 },
+	{ "bd",    						NULL,       NULL,       1 << 1,       1,           -1 },
 
-	{ "Wine",    		NULL,       NULL,       NULL, 	      1,           0 },
-	{ "Zenity",    		NULL,       NULL,       NULL, 	      1,           0 },
+	{ "Wine",    					NULL,       NULL,       NULL, 	      1,           0 },
+	{ "Zenity",    					NULL,       NULL,       NULL, 	      1,           0 },
+	{ "Gnome-calendar",    			NULL,       NULL,       NULL, 	      1,           -1 },
+	{ "Gnome-calculator",    		NULL,       NULL,       NULL, 	      1,           -1 },
 };
 
 #include "funcions/movestack.c" // функция для циклического перемещения окон
